e2d901a2afa9bfc6fbcd09f425ac83db5577eafb
JOAO PEDRO MARTINS ABDU <202401000981@alunos.ibmec.edu.br>
2024-10-17 16:43:17 -0300

    m

--- react-base-projectt/src/components/Header/Style.js
+++ react-base-projectt/src/components/Header/Style.js
@@ -6,6 +6,11 @@ const Top = styled.header`
   display: flex;
   flex-direction: row;
   margin: 0;
+  align-items: center;
+  position: sticky;
+  top: 0;
+  z-index: 1000; 
+  box-shadow: 0 2px 4px rgba(0, 0, 0, 0.1);
   a {
     text-align: center;
     padding: .3em;
@@ -17,15 +22,31 @@ const Top = styled.header`
     text-transform: uppercase;
     /*border-radius: .3em;*/
     &:hover{
-      background-color: var(--destaque);
-      color: var(--secundaria);
+      text-decoration: underline;
     }
   }
 
   .logout {
     margin-left: auto;
   }
+  
 
+  input[type="text"] {
+    padding-left: 0.5em;
+    margin-left: 8rem;
+    margin-right: 1em;
+    border: 0.5px solid transparent;
+    border-radius: 1rem;
+    height: 1.6rem;
+    width: 14rem;
+    background-color: #e0e0e0; 
+    transition: border 0.2s ease-in-out, box-shadow 0.2s ease-in-out; 
+  }
+
+  input[type="text"]:focus {
+    border: 1px solid #000;
+    box-shadow: 0 0 1px rgba(0, 0, 0, 0.2);
+  }
 `
 
 export {Top}

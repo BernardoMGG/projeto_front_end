5b63e6079d02f54e64d2845e6f2425024bdcfe77
Bryanads <bryanamorim8@gmail.com>
2024-10-26 23:26:39 -0300

    criacao da BottomBar

--- react-base-project/src/components/Header/Header.jsx
+++ react-base-project/src/components/Header/Header.jsx
@@ -1,41 +1,108 @@
-import { Link, useNavigate } from "react-router-dom";
-import { Top } from "./Style";
+// Header.js
+import { Link, useNavigate, useLocation } from "react-router-dom";
+import { Top, BottomBar } from "./Style";
 import { useState } from "react";
 
 const Header = () => {
   const [searchQuery, setSearchQuery] = useState('');
   const navigate = useNavigate();
+  const location = useLocation();
+  let botaodireita = "";
+  let linkbotaodireita = "";
 
   const handleSearchSubmit = (e) => {
     e.preventDefault();
     if (searchQuery) {
-      navigate(`/projetos/page/1?search=${encodeURIComponent(searchQuery)}`); // Ajuste para a rota de busca
+      navigate(`/projetos/page/1?search=${encodeURIComponent(searchQuery)}`);
+    }
+  };
+
+  const getPageTitle = () => {
+    const accessToken = window.sessionStorage.getItem('accessToken'); //Aproveitei pra verificar se é ADM aqui dentro dessa função logo
+
+    if (location.pathname.startsWith("/projetos")) {
+      botaodireita = accessToken ? "Adicionar Projeto" : "";
+      linkbotaodireita = "adicionar-projeto";
+      return "PROJETOS";
+    } else if (location.pathname.startsWith("/projeto")) {
+      botaodireita = accessToken ? "Editar Projeto" : "";
+      linkbotaodireita = "editar-projeto";
+      return "DETALHES DO PROJETO";
+    } else if (location.pathname === "/sobre-nos") {
+      botaodireita = "";
+      return "SOBRE NÓS";
+    } else {
+      botaodireita = accessToken ? "Adicionar Projeto" : "";
+      linkbotaodireita = "adicionar-projeto";
+      return "PROJETOS";
     }
   };
 
   return (
-    <Top>
-      <a href="https://www.ibmec.br/">
-        <img src="/imagens/logo.png" alt="ibmec" id="logo" width="auto" height="40px" />
-      </a>
-      <Link to="/projetos/page/1">Projetos</Link>
-      <Link to="/sobre-nos">Sobre Nós</Link>
-      <form onSubmit={handleSearchSubmit}>
-        <input
-          type="text"
-          id="busca"
-          placeholder="Buscar"
-          value={searchQuery}
-          onChange={(e) => setSearchQuery(e.target.value)}
-        />
-      </form>
-      {/* <div id="google_translate_element"></div> */}
-      {window.sessionStorage.getItem('accessToken') ? (
-        <Link to="/logout" className="logout">Logout</Link>
-      ) : (
-        console.log("adm logado")
-      )}
-    </Top>
+    <>
+      <Top>
+        <a href="https://www.ibmec.br/">
+          <img src="/imagens/logo.png" alt="ibmec" id="logo" width="auto" height="40px" />
+        </a>
+        <Link to="/projetos/page/1">Projetos</Link>
+        <Link to="/sobre-nos">Sobre Nós</Link>
+        <form onSubmit={handleSearchSubmit}>
+          <input
+            type="text"
+            id="busca"
+            placeholder="Buscar"
+            value={searchQuery}
+            onChange={(e) => setSearchQuery(e.target.value)}
+          />
+        </form>
+        {window.sessionStorage.getItem('accessToken')
+          ? (<Link to="/logout" className="logout">Logout</Link> )
+          : ( console.log("adm logado") )
+        }
+      </Top>
+
+      <BottomBar>
+        <div className="esquerda">
+          <span>{getPageTitle()}</span>
+          {location.pathname.startsWith("/projetos") && (
+            <div className="filtro">
+              <select>
+                <option value="" disabled selected hidden>Tecnologia ⮟</option>
+                <option value="tecnologia1">Tecnologia 1</option>
+                <option value="tecnologia2">Tecnologia 2</option>
+              </select>
+
+              <select>
+                <option value="" disabled selected hidden>Ferramenta ⮟</option>
+                <option value="ferramenta1">Ferramenta 1</option>
+                <option value="ferramenta2">Ferramenta 2</option>
+              </select>
+
+              <select>
+                <option value="" disabled selected hidden>Curso ⮟</option>
+                <option value="curso1">Curso 1</option>
+                <option value="curso2">Curso 2</option>
+              </select>
+
+              <select>
+                <option value="" disabled selected hidden>Período ⮟</option>
+                <option value="periodo1">Período 1</option>
+                <option value="periodo2">Período 2</option>
+              </select>
+
+              <select>
+                <option value="" disabled selected hidden>Unidade ⮟</option>
+                <option value="unidade1">Unidade 1</option>
+                <option value="unidade2">Unidade 2</option>
+              </select>
+            </div>
+          )}
+        </div>
+        <div className="direita">
+          {botaodireita && <Link to={`/${linkbotaodireita}`} className="botaodireita">{botaodireita}</Link>}
+        </div>
+      </BottomBar>
+    </>
   );
 }
 
--- react-base-project/src/components/Header/Style.js
+++ react-base-project/src/components/Header/Style.js
@@ -9,7 +9,7 @@ const Top = styled.header`
   align-items: center;
   position: sticky;
   top: 0;
-  z-index: 1000; 
+  z-index: 1000;
   box-shadow: 0 2px 4px rgba(0, 0, 0, 0.1);
   a {
     text-align: center;
@@ -29,7 +29,7 @@ const Top = styled.header`
   .logout {
     margin-left: auto;
   }
-  
+
 
   input[type="text"] {
     padding-left: 0.5em;
@@ -39,8 +39,8 @@ const Top = styled.header`
     border-radius: 1rem;
     height: 1.6rem;
     width: 14rem;
-    background-color: #e0e0e0; 
-    transition: border 0.2s ease-in-out, box-shadow 0.2s ease-in-out; 
+    background-color: #e0e0e0;
+    transition: border 0.2s ease-in-out, box-shadow 0.2s ease-in-out;
   }
 
   input[type="text"]:focus {
@@ -53,4 +53,77 @@ const Top = styled.header`
   }
 `
 
-export {Top}
+const BottomBar = styled.div`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  height: 4em;
+
+  .esquerda {
+    height: 100%;
+    background-color: var(--destaque);
+    flex: 1;
+    display: flex;
+    align-items: center;
+    padding-left: 3em;
+    color: #ffffff;
+    font-weight: 500;
+    font-size: 1.2em;
+  }
+
+  .direita {
+    height: 100%;
+    background-color: #ffa500;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    flex: 0.3;
+    padding-right: 1em;
+  }
+
+  .filtro {
+    display: flex;
+    gap: 1em;
+    margin-left: 2em;
+  }
+
+  .filtro select {
+    word-spacing: 0.5em;
+    padding: 0.3em 1em;
+    border-radius: 1.5em;
+    border: 1px solid #ffffff;
+    background-color: transparent;
+    color: #ffffff;
+    font-weight: bold;
+    font-size: 0.9em;
+    appearance: none;
+    outline: none;
+    cursor: pointer;
+    position: relative;
+  }
+
+  .seta{
+    background-color: red;
+    margin-left: 1em;
+  }
+
+  .filtro select option {
+    color: #000000;
+  }
+
+
+  .botaodireita{
+    padding: 0.7em;
+    border-radius: 15px;
+    text-decoration: none;
+    color: white;
+    border: 1px solid white ;
+
+}
+
+  .botaodireita:hover {
+    background-color: #d99a00;
+  }
+`;
+
+export { Top, BottomBar };
